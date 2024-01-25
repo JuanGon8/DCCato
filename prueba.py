a97964f5db9c40d8e84ecb427e58c92710535606
JuanGon8 <juan_halo61@hotmail.com>
2024-01-25 17:28:35 -0600

    archivo

---
+++ prueba.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+# Configuración de la conexión a la base de datos
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="sistema"
+)
+
+# Crear un objeto Cursor para ejecutar consultas SQL
+cursor = conexion.cursor()
+
+# Sentencia SQL para actualizar los registros en la tabla "reclutamiento"
+sql = "UPDATE reclutamiento SET fecha_reingreso = '1999-12-31', fecha_baja = '1999-12-31'"
+
+try:
+    # Ejecutar la consulta
+    cursor.execute(sql)
+
+    # Confirmar los cambios en la base de datos
+    conexion.commit()
+
+    print("Registros actualizados exitosamente.")
+
+except Exception as e:
+    # En caso de error, deshacer los cambios
+    conexion.rollback()
+    print("Error:", e)
+
+finally:
+    # Cerrar el cursor y la conexión
+    cursor.close()
+    conexion.close()
